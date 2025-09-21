0830fd158c49173a4fb4c6b6832d7b18419b1dd6
Ivanrangelpm <ivan.marcolin@sptech.school>
2025-09-21 19:55:15 -0300

    criando arquivo para subir dados raw para bucket

---
+++ envio_bucket_raw.py
@@ -0,0 +1,55 @@
+import boto3
+import os
+
+# ====================== #
+#        Config          #
+# ====================== #
+
+regiao = 'us-east-1'
+nome_bucket_raw = 'raw-beira-mar'
+
+client = boto3.client('s3', region_name=regiao)
+
+pastas_para_enviar = {
+    "/01. Dataset - Medical Appointment No Shows/raw/": "ClinicaMed/",
+    "/02. Dataset - Hair Salon No-Show/raw/": "Salao/",
+    "/03. Dataset - Meteorologia/raw/": "Clima/",
+}
+
+# ====================== #
+#      Functions         #
+# ====================== #
+
+def subir_arquivo(bucket_name, file_path_local, file_path_s3):
+    """
+    Sobe um arquivo para um bucket S3.
+    :param bucket_name: Nome do bucket de destino.
+    :param file_path_local: Caminho completo do arquivo local.
+    :param file_path_s3: Caminho e nome do arquivo no S3 (ex: 'pasta/nome_arquivo.csv').
+    """
+    try:
+        print(f"Enviando '{file_path_local}' para S3 em '{file_path_s3}'...")
+        client.upload_file(file_path_local, bucket_name, file_path_s3)
+        print(f"Upload de '{file_path_local}' concluído!")
+    except Exception as e:
+        print(f"Erro ao enviar o arquivo '{file_path_local}': {e}")
+
+# ====================== #
+#        Main            #
+# ====================== #
+
+
+for pasta_local, pasta_s3 in pastas_para_enviar.items():
+        if not os.path.isdir(pasta_local):
+            print(f"Aviso: Diretório local '{pasta_local}' não encontrado. Pulando...")
+            continue
+
+        for nome_arquivo in os.listdir(pasta_local):
+            if os.path.isfile(os.path.join(pasta_local, nome_arquivo)):
+                caminho_local_completo = os.path.join(pasta_local, nome_arquivo)
+
+                caminho_s3_completo = os.path.join(pasta_s3, nome_arquivo).replace("\\", "/")
+                
+                subir_arquivo(nome_bucket_raw, caminho_local_completo, caminho_s3_completo)
+
+print("\nProcesso de upload concluído.")
---
+++ terraform/main.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92"
+    }
+  }
+  required_version = ">= 1.2"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "raw" {
+  bucket = "raw-beira-mar"
+}
+
+resource "aws_s3_object" "raw_pastas" {
+  count  = length(var.raw_folders)
+  bucket = aws_s3_bucket.raw.id
+  key    = "${var.raw_folders[count.index]}/"
+  source = "empty_file" 
+  etag   = filemd5("empty_file")
+}
+
+resource "aws_s3_bucket" "trusted" {
+  bucket = "trusted-beira-mar"
+}
+
+resource "aws_s3_object" "trusted_pastas" {
+  count  = length(var.trusted_folders)
+  bucket = aws_s3_bucket.trusted.id
+  key    = "${var.trusted_folders[count.index]}/"
+  source = "empty_file"
+  etag   = filemd5("empty_file")
+}
+
+resource "aws_s3_bucket" "refined" {
+  bucket = "refined-beira-mar"
+}
+
+resource "aws_s3_object" "refined_pastas" {
+  count  = length(var.refined_folders)
+  bucket = aws_s3_bucket.refined.id
+  key    = "${var.refined_folders[count.index]}/"
+  source = "empty_file"
+  etag   = filemd5("empty_file")
+}
+
+variable "raw_folders" {
+  description = "Lista de pastas a serem criadas no bucket raw."
+  type        = list(string)
+  default     = ["ClinicaMed", "Salao", "Clima"]
+}
+
+variable "trusted_folders" {
+  description = "Lista de pastas a serem criadas no bucket trusted."
+  type        = list(string)
+  default     = ["ClinicaMed", "Salao", "Clima"]
+}
+
+variable "refined_folders" {
+  description = "Lista de pastas a serem criadas no bucket refined."
+  type        = list(string)
+  default     = ["ClinicaMed", "Salao", "Clima", "imagens"]
+}
